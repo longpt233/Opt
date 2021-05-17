9a8f65eca916dc4d804b1a47fce8111da33a6052
LongPT <driverofl01@gmail.com>
2021-05-17 21:21:30 +0700

    hot fix gen data

--- genData.py
+++ genData.py
@@ -18,8 +18,7 @@ def distance(i1, j1, i2, j2):
 	return int(math.sqrt((i1-i2)*(i1-i2)+(j1-j2)*(j1-j2)))+1
 
 # sinh các điểm trên vòng tròn để không có 3 điểm nào thẳng hàng
-point=[[init_random*math.cos(i*2*math.pi/(2*n+1)), init_random*math.sin(i*2*math.pi/(2*n+1))] for i in range(2*n+1)] 
-# print(point)
+point=[[init_random*math.cos(i*2*math.pi/(2*n+1)), init_random*math.sin(i*2*math.pi/(2*n+1))] for i in range(2*n+2*m+1)] 
 # đảo ngẫu nhiên vị trí các điểm trong dãy điểm
 for i in range(n*2):
 	tmp=random.randint(0, len(point)-1)
@@ -29,9 +28,9 @@ for i in range(n*2):
 
 # print(point)
 
-c=[[0 for i in range(2*n+1)] for j in range(2*n+1)]
-for i in range(2*n+1):
-	for j in range(2*n+1):
+c=[[0 for i in range(2*n+2*m+1)] for j in range(2*n+2*m+1)]
+for i in range(2*n+2*m+1):
+	for j in range(2*n+2*m+1):
 		if j!=i:
 			c[i][j]=distance(point[i][0], point[i][1], point[j][0], point[j][1])
 # print(c)
@@ -45,7 +44,7 @@ with open('data_'+str(n)+'_'+str(m)+'_'+str(K)+'.txt', 'w') as f:
 	for i in range(K):
 		f.write(str(Q[i])+' ')
 	f.write('\n')
-	for i in range(2*n+1):
-		for j in range(2*n+1):
+	for i in range(2*n+2*m+1):
+		for j in range(2*n+2*m+1):
 			f.write(str(c[i][j])+' ')
 		f.write('\n')
